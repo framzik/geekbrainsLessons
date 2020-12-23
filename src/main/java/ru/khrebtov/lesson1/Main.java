8efac5cd17f6d1858fe57c02cbc17b6ccb1efe45
framzik <framzik@gmail.com>
2020-12-23 23:41:56 +0300

    1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0; 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21; 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2; 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое) и с помощью цикла(-ов) заполнить его диагональные элементы единицами; 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета); 6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят. 7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами. Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами.

--- src/main/java/ru/khrebtov/lesson1/Main.java
+++ src/main/java/ru/khrebtov/lesson1/Main.java
@@ -3,48 +3,174 @@ package ru.khrebtov.lesson1;
 public class Main {
 
   public static void main(String[] args) {
-    byte b = 127;
-    short s = 324;
-    int i = 525;
-    long l = 123L;
-    float f = 124.5f;
-    double d = 124.5;
-    char c = 'd';
-    boolean bool = true;
+    //#1
+    int[] its = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    swap(its);
+    System.out.print("Первое задание проверка результата: ");
+    printArray(its);
+
+    //#2
+    int[] array = new int[8];
+    arrayEight(array);
+    System.out.print("Второе задание проверка результата: ");
+    printArray(array);
+
+    //3
+    int[] task3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    arrayX2(task3);
+    System.out.print("Третье задание проверка результата: ");
+    printArray(task3);
+
+    //4
+    int sideQuantity = 7;
+    int[][] squareArray = squareArray(sideQuantity);
+    System.out.println("Четвертое задание проверка результата: ");
+    printArray2X2(squareArray, sideQuantity);
+
+    //5
+    System.out.print("Пятое задание проверка результата: ");
+    maksAndMinValue(new int[]{1, 2, 3, 4, 8, -225, 12, 0, 0, 1, 0, 1, 0, 1,});
+
+    //6
+    System.out.print("Шестое задание проверка результата: ");
+    int[] equality = new int[]{2, 2, 2, 1, 2, 2, 10, 1};
+    System.out.println(isEquality(equality));
+
+    //7
+    System.out.println("Седьмое задание проверка результата: ");
+    int[] arrMove = new int[]{6, 1, 3, 5};
+    printArray(arrMove);
+    int n = 4;
+    System.out.println("n = " + n);
+    printArray(moveElements(arrMove, n));
+
   }
 
-  public static float calculate(float a, float b, float c, float d) {
-    return a * (b + (c / d));
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i]);
+    }
+    System.out.println();
   }
 
-  public static boolean isInTheLimit(int a, int b) {
-    return (a + b) >= 10 && (a + b) <= 20;
-  }
-
-  public static void positiveOrNot(int a) {
-    if (a >= 0) {
-      System.out.println("Было переданно положительное число");
-    } else {
-      System.out.println("Было переданно отрицательное число");
+  public static void printArray2X2(int[][] array, int sideQuantity) {
+    for (int i = 0; i < sideQuantity; i++) {
+      for (int y = 0; y < sideQuantity; y++) {
+        System.out.print(" " + array[i][y] + " ");
+      }
+      System.out.println();
     }
   }
 
-  //на счет а=0 ситуация не оговоренна,принял ее из задания номер 5
-  public static boolean isNegative(int a) {
-    return a < 0;
-  }
-
-  public static void consoleName(String name) {
-    System.out.println("Привет, " + name + "!");
-  }
-
-  public static void leapYear(int year) {
-    if (year % 400 == 0) {
-      System.out.println("Год " + year + " высокосный");
-    } else if (year % 4 == 0 && year % 10 != 0) {
-      System.out.println("Год " + year + " высокосный");
-    } else {
-      System.out.println("Год " + year + " не высокосный");
+  public static void swap(int[] its) {
+    for (int i = 0; i < its.length; i++) {
+      int temporary = its[i];
+      if (temporary == 1) {
+        its[i] = 0;
+      } else {
+        its[i] = 1;
+      }
     }
+  }
+
+  public static void arrayEight(int[] array) {
+    array[0] = 0;
+    for (int i = 1; i < array.length; i++) {
+      array[i] = array[i - 1] + 3;
+    }
+  }
+
+  public static void arrayX2(int[] task3) {
+    for (int i = 0; i < task3.length; i++) {
+      if (task3[i] < 6) {
+        task3[i] *= 2;
+      }
+    }
+  }
+
+  public static int[][] squareArray(int sideQuantity) {
+    int[][] squareArray = new int[sideQuantity][sideQuantity];
+    for (int i = 0; i < sideQuantity; i++) {
+      for (int y = 0; y < sideQuantity; y++) {
+        if (i == y) {
+          squareArray[i][y] = 1;
+        }
+      }
+    }
+    return squareArray;
+  }
+
+  public static void maksAndMinValue(int[] array) {
+    System.out.print("Максимальное значие в массиве: " + maksValue(array) + ". Минимальное"
+        + " значие в массиве: " + minValue(array) + ".");
+    System.out.println();
+  }
+
+  public static int maksValue(int[] array) {
+    int max = -2147483648;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+
+  public static int minValue(int[] array) {
+    int min = 2147483647;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+
+  public static boolean isEquality(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      int removal = sumLeft(array, i) - sumRight(array, i);
+      if (removal == 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int sumLeft(int[] arr, int index) {
+    int leftSum = 0;
+    for (int i = 0; i <= index; i++) {
+      leftSum += arr[i];
+    }
+    return leftSum;
+  }
+
+  public static int sumRight(int[] arr, int index) {
+    int rightSum = 0;
+    for (int i = arr.length - 1; i > index; i--) {
+      rightSum += arr[i];
+    }
+    return rightSum;
+  }
+
+  public static int[] moveElements(int[] arr, int n) {
+    if (n > 0) {
+      for (int i = 0; i < n; i++) {
+        int vault = arr[arr.length - 1];
+        for (int j = arr.length - 1; j > 0; j--) {
+          arr[j] = arr[j - 1];
+        }
+        arr[0] = vault;
+      }
+    }
+    if (n < 0) {
+      for (int i = 0; i > n; i--) {
+        int vault = arr[0];
+        for (int j = 0; j < arr.length - 1; j++) {
+          arr[j] = arr[j + 1];
+        }
+        arr[arr.length - 1] = vault;
+      }
+    }
+    return arr;
   }
 }
